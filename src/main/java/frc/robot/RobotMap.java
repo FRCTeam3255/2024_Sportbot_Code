3a2d13d23f8e0767070d692211f3a088980a8ca5
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2024-10-19 09:43:09 -0700

    init commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Contains all ports on our robot */
+public class RobotMap {
+
+    public static class mapControllers {
+        public static final int DRIVER_USB = 0;
+        public static final int OPERATOR_USB = 1;
+    }
+}
