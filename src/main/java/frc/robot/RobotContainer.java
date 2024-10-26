eef5480cc7f709ad4bdb63639c0aa5e0bef42956
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2024-10-26 16:25:19 -0700

    finished Shooter subsystem, added Prep Shooter command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,15 +9,19 @@ import com.frcteam3255.joystick.SN_XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.IntakeGround;
+import frc.robot.commands.PrepShooter;
 import frc.robot.commands.intakeHopper;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 public class RobotContainer {
   private final SN_XboxController m_driverController = new SN_XboxController(1);
   private final Intake subIntake = new Intake();
   private final Hopper subHopper = new Hopper();
+  private final Shooter subShooter = new Shooter();
   private final IntakeGround com_IntakeGround = new IntakeGround(subIntake, subHopper);
+  private final PrepShooter com_PrepShooter = new PrepShooter(subShooter);
 
   public RobotContainer() {
     configureBindings();
@@ -27,6 +31,7 @@ public class RobotContainer {
   private void configureBindings() {
     m_driverController.btn_B.whileTrue(com_IntakeGround);
     m_driverController.btn_LeftBumper.whileTrue(new intakeHopper(subHopper));
+    m_driverController.btn_X.onTrue(com_PrepShooter);
 
   }
 
