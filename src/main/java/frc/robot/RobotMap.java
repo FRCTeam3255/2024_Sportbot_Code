b6775bbc94bd862962d68606f25bff13e203f06d
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2024-10-25 17:10:09 -0700

    Added Motor IDs

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,8 +11,28 @@ public class RobotMap {
     public static final int DRIVER_USB = 0;
     public static final int OPERATOR_USB = 1;
   }
-}
 
-public static class mapStager {
-  public static final int 
+  public static class mapDriveTrain {
+    // Motor ID's
+    public static final int FRONT_RIGHT_MOTOR = 0;
+    public static final int BACK_RIGHT_MOTOR = 1;
+    public static final int FRONT_LEFT_MOTOR = 2;
+    public static final int BACK_LEFT_MOTOR = 3;
+  }
+
+  public static class mapIntake {
+    public static final int TOP_MOTOR_CAN = 20;
+    public static final int BOTTOM_MOTOR_CAN = 21;
+  }
+
+  public static class mapHopper {
+    public static final int HOPPER_MOTOR = 30;
+    public static final int GAME_PIECE_HOPPER_DIO = 1;
+  }
+
+  public static class mapShooter {
+    public static final int PROPEL_MOTOR_CAN = 10;
+    public static final int SPIN_MOTOR_CAN = 11;
+  }
+
 }
