e83e5561f1511b5c7a22373bd8bc94d200a11426
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2024-10-26 17:36:44 -0700

    remove globShooter :(
    Co-Authored-By: Wu-Fan-529 <176576491+wu-fan-529@users.noreply.github.com>

--- src/main/java/frc/robot/commands/StageGP.java
+++ src/main/java/frc/robot/commands/StageGP.java
@@ -5,13 +5,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Stager;
 
 public class StageGP extends Command {
   /** Creates a new StageGP. */
   Stager globalStager;
-  Shooter globShooter;
 
   public StageGP(Stager passedStager) {
     // Use addRequirements() here to declare subsystem dependencies.
