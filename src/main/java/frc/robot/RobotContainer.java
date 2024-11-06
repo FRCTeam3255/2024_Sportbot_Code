95edba615974e36b177b3df5f43cd8381b75b7b6
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2024-11-05 18:13:59 -0800

    Re-merged `RobotContainer.java`
    "Hopefully" correctly merged `RobotContainer.java` with the old version. ㅠㅜ

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,14 +5,15 @@
 package frc.robot;
 
 import com.frcteam3255.joystick.SN_XboxController;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.Drive;
-import frc.robot.subsystems.Drivetrain;
 import frc.robot.commands.IntakeGround;
 import frc.robot.commands.PrepShooter;
 import frc.robot.commands.StageGP;
 import frc.robot.commands.intakeHopper;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
@@ -20,22 +21,22 @@ import frc.robot.subsystems.Stager;
 
 public class RobotContainer {
   private final SN_XboxController m_driverController = new SN_XboxController(RobotMap.mapControllers.DRIVER_USB);
-
-  Drivetrain subDrivetrain = new Drivetrain();
+  private final Drivetrain subDrivetrain = new Drivetrain();
+  private final Intake subIntake = new Intake();
+  private final Hopper subHopper = new Hopper();
+  private final Shooter subShooter = new Shooter();
+  private final Stager subStager = new Stager();
+  private final Drive com_Drive = new Drive(subDrivetrain, m_driverController.axis_LeftY, m_driverController.axis_RightX);
+  private final IntakeGround com_IntakeGround = new IntakeGround(subIntake, subHopper);
+  private final PrepShooter com_PrepShooter = new PrepShooter(subShooter);
+  private final StageGP com_StageGP = new StageGP(subStager);
 
   public RobotContainer() {
-    subDrivetrain.setDefaultCommand(new Drive(subDrivetrain, m_driverController.axis_LeftY, m_driverController.axis_RightX));
-    final Intake subIntake = new Intake();
-    final Hopper subHopper = new Hopper();
-    final Shooter subShooter = new Shooter();
-    final Stager subStager = new Stager();
-    final IntakeGround com_IntakeGround = new IntakeGround(subIntake, subHopper);
-    final PrepShooter com_PrepShooter = new PrepShooter(subShooter);
-    final StageGP com_StageGP = new StageGP(subStager);
-    configureBindings(com_IntakeGround, subHopper, com_PrepShooter, com_StageGP);
+    subDrivetrain.setDefaultCommand(com_Drive);
+    configureBindings();
   }
 
-  private void configureBindings(IntakeGround com_IntakeGround, Hopper subHopper, PrepShooter com_PrepShooter, StageGP com_StageGP) {
+  private void configureBindings() {
     m_driverController.btn_B.whileTrue(com_IntakeGround);
     m_driverController.btn_LeftBumper.whileTrue(new intakeHopper(subHopper));
     m_driverController.btn_X.onTrue(com_PrepShooter);
