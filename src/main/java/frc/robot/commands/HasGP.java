b2537628564da38d35e60b73c2d2f405ae2d9404
BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>
2024-11-09 15:15:33 -0800

    removed unused if statement

--- src/main/java/frc/robot/commands/HasGP.java
+++ src/main/java/frc/robot/commands/HasGP.java
@@ -25,11 +25,7 @@ public class HasGP extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (globalStager.getIsGPStaged()) {
 
-    } else {
-
-    }
   }
 
   // Called once the command ends or is interrupted.
