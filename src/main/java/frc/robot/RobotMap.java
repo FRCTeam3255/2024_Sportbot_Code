81f33c864c788978b0c098060a83c2453096f98b
FRCTeam3255-Shared <frcteam3255@gmail.com>
2024-11-09 22:25:34 -0800

    Implemented a quick fix for drivetrain controls
    Co-Authored-By: Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -20,10 +20,10 @@ public class RobotMap {
 
   public static class mapDriveTrain {
     // Motor ID's
-    public static final int FRONT_RIGHT_MOTOR = 0;
-    public static final int BACK_RIGHT_MOTOR = 1;
-    public static final int FRONT_LEFT_MOTOR = 2;
-    public static final int BACK_LEFT_MOTOR = 3;
+    public static final int FRONT_RIGHT_MOTOR = 1;
+    public static final int BACK_RIGHT_MOTOR = 2;
+    public static final int FRONT_LEFT_MOTOR = 3;
+    public static final int BACK_LEFT_MOTOR = 4;
   }
 
   public static class mapIntake {
