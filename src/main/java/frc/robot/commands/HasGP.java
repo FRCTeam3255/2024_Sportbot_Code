8b20a2968bc9ba4b17d6251462825be303bc3736
BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>
2024-11-09 16:12:51 -0800

    removed all motor instances of motors used in the stager and renamed … (#37)
    * removed all motor instances of motors used in the stager and renamed it to hasGP🦟™️
    
    * readded the motors🦟™️
    
    * removed unused if statement

--- src/main/java/frc/robot/commands/HasGP.java
+++ src/main/java/frc/robot/commands/HasGP.java
@@ -7,11 +7,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Stager;
 
-public class StageGP extends Command {
+public class HasGP extends Command {
   /** Creates a new StageGP. */
   Stager globalStager;
 
-  public StageGP(Stager passedStager) {
+  public HasGP(Stager passedStager) {
     // Use addRequirements() here to declare subsystem dependencies.
     globalStager = passedStager;
   }
@@ -19,23 +19,19 @@ public class StageGP extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    globalStager.setStagerMotorVelocity(0.3);
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (globalStager.getHasGP()) {
-      globalStager.setStagerMotorVelocity(0);
-    } else {
-      globalStager.setStagerMotorVelocity(0.3);
-    }
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    globalStager.setStagerMotorVelocity(0);
+
   }
 
   // Returns true when the command should end.
