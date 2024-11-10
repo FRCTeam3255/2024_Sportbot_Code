ee3da3156ce7f55120d8afb4cee90d271cfa879c
Wu-Fan-529 <3681247216@qq.com>
2024-11-09 23:22:26 -0800

    change motor to TalonSRX (#44)

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,39 +4,44 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+// import com.ctre.phoenix6.hardwareTalonSRX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
-  TalonFX propelMotor;
-  TalonFX spiralMotor;
+  TalonSRX propelMotor;
+  TalonSRX spiralMotor;
 
   public Shooter() {
-    propelMotor = new TalonFX(RobotMap.mapShooter.PROPEL_MOTOR_CAN);
-    spiralMotor = new TalonFX(RobotMap.mapShooter.SPIRAL_MOTOR_CAN);
+    propelMotor = new TalonSRX(RobotMap.mapShooter.PROPEL_MOTOR_CAN);
+    spiralMotor = new TalonSRX(RobotMap.mapShooter.SPIRAL_MOTOR_CAN);
   }
 
   public void setSpiralMotorVelocity(double velocity) {
-    spiralMotor.set(velocity);
+    spiralMotor.set(ControlMode.PercentOutput, velocity);
   }
 
   public double getSpiralMotorVelocity() {
-    return spiralMotor.get();
+    return spiralMotor.getMotorOutputVoltage();
   }
 
   public void setPropelMotorVelocity(double velocity) {
-    propelMotor.set(velocity);
+    propelMotor.set(ControlMode.PercentOutput, velocity);
   }
 
   public double getPropelMotorVelocity() {
-    return propelMotor.get();
+    return propelMotor.getMotorOutputVoltage();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("propelMotorSpeed", getPropelMotorVelocity());
+    SmartDashboard.putNumber("spiralMotorSpeed", getSpiralMotorVelocity());
   }
 }
