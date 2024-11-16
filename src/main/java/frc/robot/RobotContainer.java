98290dd6811ea5c96d0671beceb1801943fc1ec8
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2024-11-15 18:38:13 -0800

    Switched the Bumpers
    thx for the suggestions tay tay

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,7 +45,7 @@ public class RobotContainer {
     m_driverController.btn_B.whileTrue(com_IntakeGround);
     m_driverController.btn_X.whileTrue(com_PrepShooter);
     m_driverController.btn_A.whileTrue(com_StageGP);
-    m_driverController.btn_RightBumper.whileTrue(com_IntakeHopper);
+    m_driverController.btn_LeftBumper.whileTrue(com_IntakeHopper);
 
     m_driverController.btn_RightBumper.whileTrue(new EjectGP(subIntake, subHopper));
 
