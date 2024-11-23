8e66ee584637fb790ff50b923fbf992eb08ef500
FRCTeam3255-Shared-K11-20 <frcteam.3255@gmail.com>
2024-11-23 13:17:31 -0800

    Update src/main/java/frc/robot/RobotContainer.java
    Co-authored-by: Tayler Uva <8679670+TaylerUva@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -47,8 +47,7 @@ public class RobotContainer {
   private void configureBindings() {
     m_driverController.btn_X.whileTrue(com_PrepShooter);
     m_driverController.btn_A.whileTrue(com_StageGP);
-    m_driverController.btn_LeftBumper.whileTrue(com_IntakeHopper);
-    m_driverController.btn_LeftBumper.whileTrue(com_IntakeGround);
+    m_driverController.btn_LeftBumper.whileTrue(com_IntakeHopper).whileTrue(com_IntakeGround);
 
     m_driverController.btn_RightBumper.whileTrue(com_EjectGP);
 
